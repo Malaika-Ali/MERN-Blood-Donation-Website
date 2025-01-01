746d889413736d54d102fe45a8d8fa6230d32b80
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-01-01 18:25:08 +0500

    changed proxy url

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
-      // '/socket.io': {
-      //   target: 'http://localhost:8000', // Proxy for Socket.IO
-      //   ws: true, // Enable WebSocket proxying
-      // },
-      '/socket.io': {
-                target: 'http://localhost:8000', // Backend URL
-                ws: true,
-                changeOrigin: true,
-            },
+      '/api': 'https://blood-donation.adaptable.app',
     }
   },
   plugins: [react()],
